05e052dc50dc74c7585be4bbe171573efe9ad207
s21290 <s21290@pjwstk.edu.pl>
2024-04-08 22:04:08 +0200

    Created user model

---
+++ src/main/java/com/rafaeldroid/WealthWise/models/User.java
@@ -0,0 +1,20 @@
+package com.rafaeldroid.WealthWise.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String email;
+    private KindUser kindUser;
+}
