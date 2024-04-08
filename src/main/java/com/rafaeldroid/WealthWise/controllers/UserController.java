6a9e787a8f33c81b26895d6c7f728c53e7972f6f
s21290 <s21290@pjwstk.edu.pl>
2024-04-08 22:08:54 +0200

    Created user controller

---
+++ src/main/java/com/rafaeldroid/WealthWise/controllers/UserController.java
@@ -0,0 +1,16 @@
+package com.rafaeldroid.WealthWise.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wealthwise/user")
+@RequiredArgsConstructor
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+
+}
